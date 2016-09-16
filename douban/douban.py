bb82b7388bacaae762e17853122c6253f53dbd25
YYShirleyYY <yhwshirley@126.com>
2016-09-16 21:07:16 +0800

    Update douban.py

--- douban/douban.py
+++ douban/douban.py
@@ -130,16 +130,7 @@ def print_book_lists_excel(book_lists,book_tag_lists):
     wb.save(save_path)
 
 if __name__ == '__main__':
-    #book_tag_lists = ['心理','判断与决策','算法','数据结构','经济','历史']
-    #book_tag_lists = ['传记','哲学','编程','创业','理财','社会学','佛教']
-    #book_tag_lists = ['思想','科技','科学','web','股票','爱情','两性']
-    #book_tag_lists = ['计算机','机器学习','linux','android','数据库','互联网']
-    #book_tag_lists = ['数学']
-    #book_tag_lists = ['摄影','设计','音乐','旅行','教育','成长','情感','育儿','健康','养生']
-    #book_tag_lists = ['商业','理财','管理']  
-    #book_tag_lists = ['名著']
-    #book_tag_lists = ['科普','经典','生活','心灵','文学']
-    #book_tag_lists = ['科幻','思维','金融']
+    #book_tag_lists里填写感兴趣的书籍主题
     book_tag_lists = ['个人管理','算法','数据结构','哲学']
     print book_tag_lists
     book_lists=do_spider(book_tag_lists)
